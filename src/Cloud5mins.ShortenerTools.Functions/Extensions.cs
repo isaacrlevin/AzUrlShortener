de5dae32b79fd0bc184d1f83e923fe3abec6c98b
Isaac Levin <isaac.r.levin@gmail.com>
2024-12-13 10:24:20 -0800

    Test Auth

---
+++ src/Cloud5mins.ShortenerTools.Functions/Extensions.cs
@@ -0,0 +1,63 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenAI;
+using System;
+using System.ClientModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloud5mins.ShortenerTools.Functions
+{
+    public static class Extensions
+    {
+        public static void AddAIServices(this FunctionsApplicationBuilder builder)
+        {
+            var inAspire = bool.Parse(Environment.GetEnvironmentVariable("IN_ASPIRE") ?? "False");
+            var useOllama = bool.Parse(Environment.GetEnvironmentVariable("USE_OLLAMA") ?? "False");
+
+            IChatClient chatClient = null;
+
+            if (inAspire)
+            {
+                if (useOllama)
+                {
+                    builder.AddKeyedOllamaSharpChatClient("chat");
+                    chatClient = builder.Services.BuildServiceProvider().GetRequiredKeyedService<IChatClient>("chat");
+                }
+                else
+                {
+                    builder.AddKeyedAzureOpenAIClient("chat");
+                    chatClient = builder.Services.BuildServiceProvider().GetRequiredKeyedService<OpenAIClient>("chat").AsChatClient("gpt-4o-mini");
+                }
+            }
+            else
+            {
+                var endpoint = Environment.GetEnvironmentVariable("AzureOpenAIEndpoint");
+                var key = Environment.GetEnvironmentVariable("AzureOpenAIKey");
+
+                if (string.IsNullOrEmpty(endpoint) || string.IsNullOrEmpty(key))
+                {
+                    throw new NotImplementedException("AzureOpenAIEndpoint and AzureOpenAIKey must be set in the environment variables");
+                }
+                else
+                {
+                    chatClient = new AzureOpenAIClient(new Uri(endpoint), new ApiKeyCredential(key)).AsChatClient("gpt-4o-mini");
+                }
+            }
+
+            if (chatClient != null)
+            {
+                builder.Services.AddChatClient(chatClient)
+                      .UseFunctionInvocation()
+                      .UseOpenTelemetry(configure: t => t.EnableSensitiveData = true)
+                      //.UseLogging()
+                      .Build();
+            }
+        }
+    }
+}
--- src/Cloud5mins.ShortenerTools.Functions/Program.cs
+++ src/Cloud5mins.ShortenerTools.Functions/Program.cs
@@ -1,6 +1,7 @@
 using Azure.AI.OpenAI;
 using Cloud5mins.ShortenerTools.Core.Domain;
 using Cloud5mins.ShortenerTools.Core.Domain.Socials.LinkedIn.Models;
+using Cloud5mins.ShortenerTools.Functions;
 using LinkedIn;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
@@ -50,48 +51,6 @@ builder.Logging.Services.Configure<LoggerFilterOptions>(options =>
 
 builder.Services.AddSingleton(options => { return shortenerSettings; });
 
-if (inAspire)
-{
-    if (useOllama)
-    {
-        builder.AddKeyedOllamaSharpChatClient("chat");
-        var sp = builder.Services.BuildServiceProvider();
-
-        builder.Services.AddChatClient(sp.GetRequiredKeyedService<IChatClient>("chat"))
-                  .UseFunctionInvocation()
-                  .UseOpenTelemetry(configure: t => t.EnableSensitiveData = true)
-                  //.UseLogging()
-                  .Build();
-    }
-    else
-    {
-        builder.AddKeyedAzureOpenAIClient("chat");
-        var sp = builder.Services.BuildServiceProvider();
-
-        builder.Services.AddChatClient(sp.GetRequiredKeyedService<OpenAIClient>("chat").AsChatClient("gpt-4o-mini"))
-                  .UseFunctionInvocation()
-                  .UseOpenTelemetry(configure: t => t.EnableSensitiveData = true)
-                  //.UseLogging()
-                  .Build();
-    }
-}
-else
-{
-    var endpoint = Environment.GetEnvironmentVariable("AzureOpenAIEndpoint");
-    var key = Environment.GetEnvironmentVariable("AzureOpenAIKey");
-
-    if (string.IsNullOrEmpty(endpoint) || string.IsNullOrEmpty(key))
-    {
-
-    }
-    else
-    {
-        builder.Services.AddChatClient(new AzureOpenAIClient(new Uri(endpoint), new ApiKeyCredential(key)).AsChatClient("gpt-4o-mini"))
-                  .UseFunctionInvocation()
-                  .UseOpenTelemetry(configure: t => t.EnableSensitiveData = true)
-                  //.UseLogging()
-                  .Build();
-    }
-}
+builder.AddAIServices();
 
 builder.Build().Run();
