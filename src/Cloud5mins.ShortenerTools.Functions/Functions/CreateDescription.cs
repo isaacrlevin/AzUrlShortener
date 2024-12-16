b361152476455e65c6f5a5c99a2ea0af8dadf5d9
Isaac Levin <isaac.r.levin@gmail.com>
2024-12-16 11:28:24 -0800

    Update Message Generation

--- src/Cloud5mins.ShortenerTools.Functions/Functions/CreateDescription.cs
+++ src/Cloud5mins.ShortenerTools.Functions/Functions/CreateDescription.cs
@@ -1,11 +1,9 @@
 using Cloud5mins.ShortenerTools.Core.Messages;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Logging;
-using OllamaSharp;
 using System.Net;
 using System.Text.Json;
 
@@ -26,8 +24,6 @@ namespace Cloud5mins.ShortenerTools.Functions.Functions
         public async Task<HttpResponseData> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "api/CreateDescription")] HttpRequestData req)
         {
-            var result = string.Empty;
-            var url = string.Empty;
             try
             {
                 // Validation of the inputs
@@ -36,18 +32,20 @@ namespace Cloud5mins.ShortenerTools.Functions.Functions
                     return req.CreateResponse(HttpStatusCode.NotFound);
                 }
 
+                ShortUrlRequest shortUrlRequest = new ShortUrlRequest();
+
                 using (var reader = new StreamReader(req.Body))
                 {
                     var strBody = await reader.ReadToEndAsync();
-                    url = JsonSerializer.Deserialize<string>(strBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                    if (string.IsNullOrEmpty(url))
+                    shortUrlRequest = JsonSerializer.Deserialize<ShortUrlRequest>(strBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    if (string.IsNullOrEmpty(shortUrlRequest.Url))
                     {
                         return req.CreateResponse(HttpStatusCode.NotFound);
                     }
                 }
 
                 // If the Url parameter only contains whitespaces or is empty return with BadRequest.
-                if (string.IsNullOrWhiteSpace(url))
+                if (string.IsNullOrWhiteSpace(shortUrlRequest.Url))
                 {
                     var badResponse = req.CreateResponse(HttpStatusCode.BadRequest);
                     await badResponse.WriteAsJsonAsync(new { Message = "The url parameter can not be empty." });
@@ -55,14 +53,33 @@ namespace Cloud5mins.ShortenerTools.Functions.Functions
                 }
 
                 // Validates if input.url is a valid aboslute url, aka is a complete refrence to the resource, ex: http(s)://google.com
-                if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                if (!Uri.IsWellFormedUriString(shortUrlRequest.Url, UriKind.Absolute))
                 {
                     var badResponse = req.CreateResponse(HttpStatusCode.BadRequest);
-                    await badResponse.WriteAsJsonAsync(new { Message = $"{url} is not a valid absolute Url. The Url parameter must start with 'http://' or 'http://'." });
+                    await badResponse.WriteAsJsonAsync(new { Message = $"{shortUrlRequest.Url} is not a valid absolute Url. The Url parameter must start with 'http://' or 'http://'." });
                     return badResponse;
                 }
 
+                int contentLength = 0;
 
+                if (!string.IsNullOrEmpty(shortUrlRequest.Title))
+                {                    
+                    contentLength += shortUrlRequest.Title.Length;
+                }
+
+                string shortUrlTemplate = "https://isaacl.dev/aaaa";
+                contentLength += shortUrlTemplate.Length;
+
+
+                var message = @$"Create a professional social media post for this link with proper hastags. 
+                                 Do not include the link in the response or the title of the page. Only return meaningful content regarding page referenced, 
+                                 nothing else in the response. The entire response should not exceed {280 - contentLength - 3} characters. {shortUrlRequest.Url}";
+                var chatResponse = await _client.CompleteAsync(message);
+
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                await response.WriteAsJsonAsync(chatResponse.Message.Text);
+
+                return response;
             }
             catch (Exception ex)
             {
@@ -72,13 +89,6 @@ namespace Cloud5mins.ShortenerTools.Functions.Functions
                 await badResponse.WriteAsJsonAsync(new { ex.Message });
                 return badResponse;
             }
-            var message = $"Create a professional social media post for this link with proper hastags. Do not include the link in the post or the title of the page. Only return the post, nothing else in the response. The entire post, including the title should not exceed 120 characters. {url}";
-            var chatResponse = await _client.CompleteAsync(message);
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            await response.WriteAsJsonAsync(chatResponse.Message.Text);
-
-            return response;
         }
     }
 }
